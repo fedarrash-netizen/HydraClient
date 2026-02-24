601e2882be495096944faa0bfc958fd373e5d576
fedarrash-netizen <feadr.rash@gmail.com>
2026-02-24 21:15:41 +0300

    Fix LocalPlayerMixin crash and update ColorSetting with HSB support

--- src/client/java/win/winlocker/DropDown/settings/ColorSetting.java
+++ src/client/java/win/winlocker/DropDown/settings/ColorSetting.java
@@ -2,10 +2,12 @@ package win.winlocker.DropDown.settings;
 
 public class ColorSetting extends Setting {
     private int color;
+    private final int defaultColor;
 
     public ColorSetting(String name, int defaultColor) {
         super(name);
         this.color = defaultColor;
+        this.defaultColor = defaultColor;
     }
 
     public int get() {
@@ -14,5 +16,63 @@ public class ColorSetting extends Setting {
 
     public void set(int color) {
         this.color = color;
+    }
+
+    public void reset() {
+        this.color = defaultColor;
+    }
+
+    public int getDefault() {
+        return defaultColor;
+    }
+
+    public int getRed() {
+        return (color >> 16) & 0xFF;
+    }
+
+    public int getGreen() {
+        return (color >> 8) & 0xFF;
+    }
+
+    public int getBlue() {
+        return color & 0xFF;
+    }
+
+    public int getAlpha() {
+        return (color >> 24) & 0xFF;
+    }
+
+    public void setRGB(int r, int g, int b) {
+        int a = getAlpha();
+        this.color = (a << 24) | (r << 16) | (g << 8) | b;
+    }
+
+    public void setRGBA(int r, int g, int b, int a) {
+        this.color = (a << 24) | (r << 16) | (g << 8) | b;
+    }
+
+    public float getHue() {
+        return rgbToHsb(color)[0];
+    }
+
+    public float getSaturation() {
+        return rgbToHsb(color)[1];
+    }
+
+    public float getBrightness() {
+        return rgbToHsb(color)[2];
+    }
+
+    public void setHSB(float h, float s, float b) {
+        int rgb = java.awt.Color.HSBtoRGB(h, s, b);
+        int a = getAlpha();
+        this.color = (a << 24) | (rgb & 0xFFFFFF);
+    }
+
+    private float[] rgbToHsb(int rgb) {
+        float r = (float) ((rgb >> 16) & 0xFF) / 255.0f;
+        float g = (float) ((rgb >> 8) & 0xFF) / 255.0f;
+        float b = (float) (rgb & 0xFF) / 255.0f;
+        return java.awt.Color.RGBtoHSB((int) (r * 255), (int) (g * 255), (int) (b * 255), null);
     }
 }
--- src/client/java/win/winlocker/mixin/client/FogRendererMixin.java
+++ src/client/java/win/winlocker/mixin/client/FogRendererMixin.java
@@ -2,15 +2,12 @@ package win.winlocker.mixin.client;
 
 import net.minecraft.client.renderer.FogRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import win.winlocker.module.render.CustomFog;
 
 @Mixin(FogRenderer.class)
 public class FogRendererMixin {
 
-    @Inject(method = "getFogStart", at = @At("RETURN"), cancellable = true)
     private static void onGetFogStart(CallbackInfoReturnable<Float> cir) {
         CustomFog customFog = CustomFog.getInstance();
         if (customFog != null && customFog.isEnabled()) {
@@ -22,7 +19,6 @@ public class FogRendererMixin {
         }
     }
 
-    @Inject(method = "getFogEnd", at = @At("RETURN"), cancellable = true)
     private static void onGetFogEnd(CallbackInfoReturnable<Float> cir) {
         CustomFog customFog = CustomFog.getInstance();
         if (customFog != null && customFog.isEnabled()) {
