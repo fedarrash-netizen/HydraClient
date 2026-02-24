f0de080a7997675c162317394d76a895f0643f51
fedarrash-netizen <feadr.rash@gmail.com>
2026-02-24 21:39:39 +0300

    Fix CustomFog: remove broken mixin, keep only ColorSetting

--- src/client/java/win/winlocker/module/render/CustomFog.java
+++ src/client/java/win/winlocker/module/render/CustomFog.java
@@ -1,25 +1,20 @@
 package win.winlocker.module.render;
 
-import win.winlocker.DropDown.settings.BooleanSetting;
 import win.winlocker.DropDown.settings.ModeSetting;
-import win.winlocker.DropDown.settings.SliderSetting;
 import win.winlocker.DropDown.settings.ColorSetting;
 import win.winlocker.module.Module;
-import win.winlocker.utils.render.DisplayUtils;
 
 import java.util.List;
 
 public class CustomFog extends Module {
     private static CustomFog instance;
 
-    public final SliderSetting power = new SliderSetting("Сила", 20.0, 1.0, 100.0);
     public final ModeSetting mode = new ModeSetting("Мод", "Клиент", List.of("Клиент", "Свой"));
     public final ColorSetting color = new ColorSetting("Цвет", 0xFF5AA8FF);
 
     public CustomFog() {
         super("CustomFog", Category.RENDER);
         instance = this;
-        addSetting(power);
         addSetting(mode);
         addSetting(color);
     }
@@ -44,10 +39,6 @@ public class CustomFog extends Module {
 
     public boolean isCustomMode() {
         return isEnabled() && "Свой".equals(mode.get());
-    }
-
-    public double getPower() {
-        return power.get();
     }
 
     public int getColor() {
